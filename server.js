029683c5f22f1fe79b7d4e4745e15ddaa18144ce
Tania <tanner.shirkey@gmail.com>
2024-06-08 18:36:21 -0400

    wrote connection and entrypoint files

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const database = require("./config/connection");
+// const routes = require("./routes");
+
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const activity = cwd.includes("Social Media Database") ? cwd.split("Social-Media-Database")[1] : cwd;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+database.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  })
+});
